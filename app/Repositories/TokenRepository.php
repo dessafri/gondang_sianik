326bba644220e96e7ff7c2cd84007f27b8ac22a7
nurulazizah0903 <incesazizah@gmail.com>
2024-01-22 11:19:22 +0700

    up

--- app/Repositories/TokenRepository.php
+++ app/Repositories/TokenRepository.php
@@ -76,7 +76,7 @@ class TokenRepository
         $curl_message = curl_init();
 
             curl_setopt_array($curl_message, array(
-            CURLOPT_URL => 'http://lasmini.cloud//api/sendMessage',
+            CURLOPT_URL => 'http://lasmini.cloud/api/sendMessage',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
